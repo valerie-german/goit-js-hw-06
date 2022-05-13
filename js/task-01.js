a7d2f3ed001b7dac15868ebbf824be0f797dcc12
Valeria German <94998541+valerie-german@users.noreply.github.com>
2022-05-14 01:24:04 +0300

    refactoring

--- js/task-01.js
+++ js/task-01.js
@@ -5,9 +5,10 @@ console.log(`Number of categories: ${categories.length}`);
 const items = document.querySelectorAll("h2");
 
 items.forEach(item => {    
-    const list = item.nextElementSibling;
+    // const list = item.nextElementSibling;
+    const list = item.parentElement.querySelectorAll("ul > li");
    
     console.log(`Category: ${item.textContent}`);
-    console.log(`Elements: ${list.children.length}`);   
+    console.log(`Elements: ${list.length}`);   
     
 });
