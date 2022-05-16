225a804d91dc55036acbd4a70b382a73d0b8a726
Valeria German <94998541+valerie-german@users.noreply.github.com>
2022-05-16 21:47:10 +0300

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector("body");
+const btn = document.querySelector("button.change-color");
+
+
+const changesBodyColor = () => {
+  body.style.backgroundColor = getRandomHexColor();
+  document.querySelector("span.color").textContent = getRandomHexColor();
+}
+btn.addEventListener("click",changesBodyColor);
