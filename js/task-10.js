2d73ef2458b5e0ac01195e35175cc1c2f4d42fcc
Valeria German <94998541+valerie-german@users.noreply.github.com>
2022-05-16 23:33:49 +0300

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,40 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const btnCreate = document.querySelector("button[data-create]");
+const btnDestroy = document.querySelector("button[data-destroy]");
+const divList = document.querySelector("#boxes");
+const input = document.querySelector("input");
+
+let inputValue = 0;
+// ---------
+function createBoxes(amount) { 
+  let boxesArray = [];  
+   for (let i = 1; i <= amount; i+=1) {
+    const div = `<div style="background-color: ${getRandomHexColor()}; width:${30*i}px; height: ${30*i}px"></div>`;
+    boxesArray.push(div);
+   }
+  console.log(boxesArray);
+  return boxesArray;
+}
+// -----------
+btnCreate.addEventListener("click", (event) => {
+
+  divList.innerHTML = createBoxes(input.value).join("");
+});
+
+btnDestroy.addEventListener("click", (event) => {
+  inputValue = event.currentTarget.value;
+  console.log(inputValue);
+
+  divList.innerHTML = "";
+
+});
+
+
+
+
+
+
