48581e8c90b313fe89bfdff86b58b53bb092c1f9
Valeria German <94998541+valerie-german@users.noreply.github.com>
2022-05-16 20:39:49 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+  
+    let object = { email: email.value, password: password.value };
+
+  if (email.value === "" || password.value === "") {
+    return alert(`Пожалуйста, не оставляйте поля пустыми. Им грустно 😢`);
+  }
+
+  console.log(object);
+  event.currentTarget.reset();  
+    
+}
